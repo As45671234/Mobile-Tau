384a1f743422a54f229e05798f0093f08b3ac1af
As45671234 <83564736+As45671234@users.noreply.github.com>
2025-05-04 17:25:19 +0500

    Add files via upload

--- js.js
+++ js.js
@@ -10,8 +10,11 @@ window.addEventListener('DOMContentLoaded', () => {
   function toggleTheme() {
     document.body.classList.toggle('light');
     const icon = document.getElementById('theme-icon');
+    const logo = document.getElementById('logo');
     const isLight = document.body.classList.contains('light');
     icon.src = isLight ? 'img/icon1.png' : 'img/icon2.png';
+    logo.src = isLight ? "img/logo.jpg" : "img/logo1.jpg";
+    
   }
   function toggleMobileNav() {
     const nav = document.getElementById('mobileNav');
